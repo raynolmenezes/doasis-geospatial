adeb97975ad524858f17f5001b6f77f0dff8192d
raynolmenezes <raynol.ec18@sahyadri.edu.in>
2022-04-03 10:26:35 +0530

    Updated frontend

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/alashqan/dOASIS"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          dOASIS - Decentralized &amp; Organized Archiving System for Information Storage
         </a>
       </header>
     </div>
